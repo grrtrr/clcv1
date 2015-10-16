db54784ffce174d9158f73e04d0460dadfed4388
Gerrit Renker <Gerrit.Renker@ctl.io>
2015-10-16 09:20:30 -0600

    Query old password if not given

--- examples/server/change_credentials.go
+++ examples/server/change_credentials.go
@@ -25,7 +25,7 @@ func main() {
 
 	flag.Parse()
 
-	if flag.NArg() != 1 || *oldPasswd == "" || *newPasswd == "" {
+	if flag.NArg() != 1 || *newPasswd == "" {
 		flag.Usage()
 		os.Exit(1)
 	}
@@ -37,9 +37,20 @@ func main() {
 		exit.Fatalf("Login failed: %s", err)
 	}
 
+	if *oldPasswd == "" {
+		fmt.Printf("Retrieving existing password for %s ...\n", flag.Arg(0))
+		creds, err := client.GetServerCredentials(flag.Arg(0), *acctAlias)
+		if err != nil {
+			exit.Fatalf("Failed to obtain credentials for %s: %s", flag.Arg(0), err)
+		}
+		*oldPasswd = creds.Password
+	}
+
+	fmt.Printf("Changing password on %s from <%v> to <%v> ...\n", flag.Arg(0), *oldPasswd, *newPasswd)
+
 	err = client.ServerChangePassword(flag.Arg(0), *acctAlias, *oldPasswd, *newPasswd)
 	if err != nil {
-		exit.Fatalf("Failed to change the password on %q: %s", flag.Arg(0), err)
+		exit.Fatalf("Failed to change the password on %s: %s", flag.Arg(0), err)
 	}
 
 	fmt.Printf("Successfully changed the password on %s to %s\n", flag.Arg(0), *newPasswd)
