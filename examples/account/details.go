3b73bd3c2a7cf25ae88cd716f4b1a71ac4367ee1
Gerrit Renker <gerrit@erg.abdn.ac.uk>
2015-12-12 13:14:02 -0700

    Add missing usage

--- examples/account/details.go
+++ examples/account/details.go
@@ -7,15 +7,24 @@ import (
 	"github.com/grrtrr/clcv1/utils"
 	"github.com/grrtrr/clcv1"
 	"github.com/grrtrr/exit"
+	"path"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 )
 
 func main() {
-	var acctAlias = flag.String("a", "", "Account alias to use")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "usage: %s <Account-Alias>\n", path.Base(os.Args[0]))
+		flag.PrintDefaults()
+	}
 
 	flag.Parse()
+	if flag.NArg() != 1 {
+		flag.Usage()
+		os.Exit(0)
+	}
 
 	client, err := clcv1.NewClient(log.New(os.Stdout, "", log.LstdFlags | log.Ltime))
 	if err != nil {
@@ -24,9 +33,9 @@ func main() {
 		exit.Fatalf("Login failed: %s", err)
 	}
 
-	acctDetails, err := client.GetAccountDetails(*acctAlias)
+	acctDetails, err := client.GetAccountDetails(flag.Arg(0))
 	if err != nil {
-		exit.Fatalf("Failed to obtain account details of %s: %s", *acctAlias, err)
+		exit.Fatalf("Failed to obtain account details of %s: %s", flag.Arg(0), err)
 	}
 
 	utils.PrintStruct(acctDetails)
