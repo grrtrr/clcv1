a12307a2787e7dd7d821e08f8ab8ccdb2c8e8bd1
Gerrit Renker <Gerrit.Renker@ctl.io>
2016-03-18 10:08:23 -0600

    update documentation

--- blueprint.go
+++ blueprint.go
@@ -1,11 +1,12 @@
 package clcv1
 
 import (
-	"github.com/grrtrr/clcv1/microsoft"
-	"github.com/dustin/go-humanize"
+	"fmt"
 	"strings"
 	"time"
-	"fmt"
+
+	"github.com/dustin/go-humanize"
+	"github.com/grrtrr/clcv1/microsoft"
 )
 
 /*
@@ -17,35 +18,35 @@ type SearchBlueprintReq struct {
 	// 2 = 101 - 1,000
 	// 3 = 1,001 - 5,000
 	// 4 = 5,000+
-	CompanySize		int
+	CompanySize int
 
 	// A list of the operating systems that a Blueprint contains (optional)
 	// e.g. Cent OS - 32 bit = 6,  Cent OS - 64 bit = 7
-	OperatingSystems	[]int
+	OperatingSystems []int
 
 	// A keyword search within the Name and Description of the Blueprint (optional)
-	Search			string
+	Search string
 
 	// The visibility level of the Blueprint.
 	// 1 = Public
 	// 2 = Private
 	// 3 = Private Shared
-	Visibility		int
+	Visibility int
 }
 
 type Blueprint struct {
 	// The ID of the Blueprint.
-	ID	int
+	ID int
 	// The name of the Blueprint.
-	Name	string
+	Name string
 }
 
 // Get a list of all Blueprints with the specified search criteria.
 func (c *Client) GetBlueprints(req *SearchBlueprintReq) (blueprints []Blueprint, err error) {
 	err = c.getResponse("/Blueprint/GetBlueprints/JSON", req, &struct {
 		BaseResponse
-		Blueprints	*[]Blueprint
-	} { Blueprints: &blueprints })
+		Blueprints *[]Blueprint
+	}{Blueprints: &blueprints})
 	return
 }
 
@@ -54,26 +55,26 @@ func (c *Client) GetBlueprints(req *SearchBlueprintReq) (blueprints []Blueprint,
  */
 type DeploymentStatus struct {
 	// The ID of the Blueprint deployment request. (Obtained from DeployBlueprint.)
-	RequestID	int
+	RequestID int
 
 	// The status of the Blueprint deployment. Valid values are: NotStarted, Executing, Succeeded, Failed, and Resumed.
-	CurrentStatus	string
+	CurrentStatus string
 
 	// A detailed description of the current status.
-	Description	string
+	Description string
 
 	// The percentage of the work that has been completed on the request.
-	PercentComplete	int
+	PercentComplete int
 
 	// The timestamp (UTC) that the most recent status was recorded on the Request.
-	StatusDate	microsoft.Timestamp
+	StatusDate microsoft.Timestamp
 
 	// The current step being executed.
-	Step		string
+	Step string
 
 	// An array of strings with the names of any servers built as part of the Blueprint.
 	// This element is only populated when the Request was originally for server creation.
-	Servers		[]string
+	Servers []string
 }
 
 // Get the status of the specified Blueprint deployment.
@@ -83,22 +84,21 @@ type DeploymentStatus struct {
 //             If not provided, will default to the API user's default data center.
 func (c *Client) GetDeploymentStatus(reqId int, acctAlias, location string) (s *DeploymentStatus, err error) {
 	req := struct {
-		RequestID	int
-		AccountAlias	string
-		LocationAlias	string
-
-	} { reqId, acctAlias, location }
+		RequestID     int
+		AccountAlias  string
+		LocationAlias string
+	}{reqId, acctAlias, location}
 	s = new(DeploymentStatus)
 	err = c.getResponse("/Blueprint/GetDeploymentStatus/JSON", &req, &struct {
 		BaseResponse
 		*DeploymentStatus
-	} { DeploymentStatus: s })
+	}{DeploymentStatus: s})
 	return
 }
 
-// Poll the queue status of @reqId each @interval seconds, until it reaches 100%
-// @reqId, @location, @acctAlias: As per GetDeploymentStatus().
-// @pollInterval:                 Poll interval in seconds; use 0 for one-off.
+// PollDeploymentStatus polls the queue status of @reqId each @pollInterval seconds, until it reaches 100%.
+// @reqId, @location, @acctAlias: as per GetDeploymentStatus().
+// @pollInterval:                 poll interval in seconds; use 0 for one-off.
 func (c *Client) PollDeploymentStatus(reqId int, location, acctAlias string, pollInterval int) error {
 	for {
 		status, err := c.GetDeploymentStatus(reqId, acctAlias, location)
@@ -109,7 +109,7 @@ func (c *Client) PollDeploymentStatus(reqId int, location, acctAlias string, pol
 		/* Clear line */
 		fmt.Printf("\r\033[2K")
 		fmt.Printf("#%d %d%% at %s (%s)", status.RequestID, status.PercentComplete,
-			   status.StatusDate.Format(time.Kitchen), humanize.Time(status.StatusDate.Time))
+			status.StatusDate.Format(time.Kitchen), humanize.Time(status.StatusDate.Time))
 		if len(status.Servers) == 1 {
 			fmt.Printf(", %s", status.Servers[0])
 		} else if len(status.Servers) > 1 {
